4594dd50608ecf37ac0b497f983efd55ce19139b
Jonathan Chandra <j1chandr@ucsd.edu>
2020-10-14 14:46:00 -0700

    Add react semanti-ui and css

--- what2watch-client-app/src/App.tsx
+++ what2watch-client-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import { Header, Icon, List } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import axios from 'axios';
 
@@ -12,7 +13,6 @@ class App extends Component{
   componentDidMount(){
     axios.get('http://localhost:5000/api/values').then((response) =>
       {
-        console.log(response)
         this.setState(
           {
             values: response.data
@@ -24,15 +24,18 @@ class App extends Component{
   render() {
 
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {this.state.values.map((value: any) => (
-            <li key={value.id}>{value.name}</li>
-          ))}
-        </ul>
-      </header>
+    <div>
+        <Header as='h2'>
+          <Icon name='user' />
+          <Header.Content>
+            What2Watch
+          </Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) =>
+            <List.Item key = {value.id}>{value.name}</List.Item>
+          )}
+        </List>
     </div>
   );
   }
